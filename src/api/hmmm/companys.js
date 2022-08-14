bec2b158803dcbe2b6363a029351ab182720f1f0
zqxtyl <2571756029@qq.com>
2022-08-14 17:21:32 +0800

    完成了企业管理

--- src/api/hmmm/companys.js
+++ src/api/hmmm/companys.js
@@ -7,12 +7,15 @@
  * 企业管理
  */
 
-import { createAPI } from '@/utils/request'
+import { createAPI } from "@/utils/request";
 
-export const list = data => createAPI('/companys', 'get', data)
-export const add = data => createAPI('/companys', 'post', data)
-export const update = data => createAPI(`/companys/${data.id}`, 'put', data)
-export const remove = data => createAPI(`/companys/${data.id}`, 'delete', data)
-export const detail = data => createAPI(`/companys/${data.id}`, 'get', data)
-export const disabled = data =>
-  createAPI(`/companys/${data.id}/state`, 'post', data)
+export const list = (data) => createAPI("/companys", "get", data);
+export const add = (data) => createAPI("/companys", "post", data);
+export const update = (data) => createAPI(`/companys/${data.id}`, "put", data);
+export const remove = (data) =>
+  createAPI(`/companys/${data.id}`, "delete", data);
+export const detail = (data) => createAPI(`/companys/${data.id}`, "get", data);
+export const disabled = (data) =>
+  createAPI(`/companys/${data.id}/state`, "post", data);
+export const changeStatus = (id, state) =>
+  createAPI(`/companys/${id}/${state}`, "post");
